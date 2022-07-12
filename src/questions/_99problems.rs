1dcb2842ed0ad3e7afe2dcecb0ff60b890de38f1
Zechariah <zechariahtan144@gmail.com>
2022-07-13 02:29:25 +0800

    ♻️ 99problems

--- src/questions/_99problems.rs
+++ src/questions/_99problems.rs
@@ -1,13 +1,14 @@
-use std::io::{stdin, stdout, Write};
+pub fn get_input() -> String {
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read input");
+    input.trim().to_string()
+}
 
 #[allow(dead_code)]
 pub fn main() {
-    let mut input = String::new();
-    stdout().flush().unwrap();
-    stdin().read_line(&mut input).unwrap();
-    let input = input.trim();
-
-    let num = input.parse::<i32>().unwrap();
+    let num = get_input().parse::<i32>().unwrap();
     if num % 100 >= 49 || num / 100 == 0 {
         println!("{}", (((num / 100) + 1) * 100) - 1);
     } else {
