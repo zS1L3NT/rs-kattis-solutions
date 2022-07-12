77747cedf99104099f3b5c197bc16c98c0c6340b
Zechariah Tan <zechariahtan144@gmail.com>
2022-07-12 22:43:37 +0800

    ⏪ Revert "✨ runtime error, 1/41"
    This reverts commit f961b4677450958a7497279fb14582300f5a7d48.

--- src/questions/sequence.rs
+++ src/questions/sequence.rs
@@ -15,31 +15,14 @@ pub fn main() {
 }
 
 fn get_possible_variations(string: String) -> Vec<String> {
-    let str_iter = string.chars().into_iter();
-    let qms = string.chars().filter(|&c| c == '?').count() as u32;
-    let mut variations = vec![];
-
-    for i in 0..(u32::pow(2, qms) as usize) {
-        let mut replaced = 0;
-        let binary = format!("{:b}", i).chars().collect::<Vec<_>>();
-        let binary = [vec!['0'; (qms - binary.len() as u32) as usize], binary].concat();
-
-        variations.push(
-            str_iter
-                .clone()
-                .map(|c| {
-                    if c == '?' {
-                        replaced += 1;
-                        binary[replaced - 1]
-                    } else {
-                        c
-                    }
-                })
-                .collect::<String>(),
-        );
+    match string.find(|char| char == '?') {
+        Some(_) => [
+            get_possible_variations(string.replacen("?", "0", 1)),
+            get_possible_variations(string.replacen("?", "1", 1)),
+        ]
+        .concat(),
+        None => vec![string],
     }
-
-    variations
 }
 
 fn count_iterations(string: &mut String) -> usize {
